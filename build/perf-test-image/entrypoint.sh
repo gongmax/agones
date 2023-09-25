ae4dd62aabbf7bc45bbaa28af87449accd37add3
gongmax <gongmax@google.com>
2023-09-25 18:30:23 +0000

    fix

---
+++ build/perf-test-image/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Copyright 2018 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -e
+TEST_CLUSTER_NAME=$1
+TEST_CLUSTER_LOCATION=$2
+
+export SHELL="/bin/bash"
+export KUBECONFIG="/root/.kube/config"
+mkdir -p /go/src/agones.dev/ /root/.kube/
+ln -s /workspace /go/src/agones.dev/agones
+cd /go/src/agones.dev/agones/build
+if [ "$1" = 'local' ]
+then
+        gcloud auth login
+fi
+gcloud container clusters get-credentials $TEST_CLUSTER_NAME \
+        --zone=${TEST_CLUSTER_LOCATION} --project=gongmax-gke-dev
