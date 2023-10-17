e237e5d1572bf3e07b4ff6cc4a3ef545bb2d6602
gongmax <gongmax@google.com>
2023-10-17 21:33:05 +0000

    fix

--- build/performance-test.sh
+++ build/performance-test.sh
@@ -20,7 +20,7 @@ mkdir -p /go/src/agones.dev/
 ln -s /workspace /go/src/agones.dev/agones
 cd /go/src/agones.dev/agones/build
 echo "!!!!!!"
-echo $PROJECT
+echo $CLUSTER_NAME
 
 gcloud config set project $PROJECT
 gcloud container clusters get-credentials $CLUSTER_NAME \
